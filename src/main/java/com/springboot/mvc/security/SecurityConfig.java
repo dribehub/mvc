554371654fd616369c222ad819d9a9dde08ef56e
dribe <73489903+dribehub@users.noreply.github.com>
2021-08-11 10:36:32 +0200

    minor fixes

--- src/main/java/com/springboot/mvc/security/SecurityConfig.java
+++ src/main/java/com/springboot/mvc/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.springboot.mvc.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,36 +13,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-            .withUser("aretha.sydney@gmail.com")
-                .password(passwordEncoder().encode("aretha.sydney"))
-                .roles("user").and()
-            .withUser("michael.hall@gmail.com")
-                .password(passwordEncoder().encode("michael.hall"))
-                .roles("user").and()
-            .withUser("david.reed@gmail.com")
-                .password(passwordEncoder().encode("david.reed"))
-                .roles("user").and()
-            .withUser("linda.baker@gmail.com")
-                .password(passwordEncoder().encode("linda.baker"))
-                .roles("user").and()
-            .withUser("william.ford@gmail.com")
-                .password(passwordEncoder().encode("william.ford"))
-                .roles("user").and()
-            .withUser("beder.butka@gmail.com")
-                .password(passwordEncoder().encode("beder.butka"))
-                .roles("user", "admin");
-    }
-
-    @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .antMatchers("/").hasRole("admin")
-            .antMatchers("/**").permitAll()
-            .anyRequest().authenticated().and()
-            .formLogin().loginPage("/signup").permitAll();
-//            .and().logout().logoutUrl("/logout").permitAll();
+                .antMatchers("/signup").permitAll()
+                .antMatchers("/**").permitAll()
+                .antMatchers("/").hasAnyRole("admin", "user")
+                .anyRequest().authenticated().and()
+                .formLogin().loginPage("/login").permitAll();
+//                .and().logout().logoutUrl("/logout").permitAll();
     }
 
     @Bean
